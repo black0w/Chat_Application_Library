92451b064d9912e728e45c582d4012178e99676e
dian andreev georgiev <arrwblack@gmail.com>
2021-07-28 02:40:44 +0300

    added GroupChatResponse for sending group chats to client

---
+++ MessageTypes/Chat/GroupChatResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chat_Application_Library
+{
+   public class GroupChatResponse : Message
+    {
+        List<int> GroupIds { get; set; }
+        public GroupChatResponse()
+        {
+            Type = Type.Response;
+            Action = "GroupChat";
+        }
+    }
+}
