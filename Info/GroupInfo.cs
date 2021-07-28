cdb5a8f9c126268471f8758450497656e7b6d0b9
dian andreev georgiev <arrwblack@gmail.com>
2021-07-28 15:55:55 +0300

    bugfix

--- Info/GroupInfo.cs
+++ Info/GroupInfo.cs
@@ -8,8 +8,7 @@ namespace Chat_Application_Library
     {
         public List<UserInfo> Members { get; set; }
         public List<MessageInfo> Messages { get; set; }
-        public DateTime LastSentMessageTimeStamp { get; set; }
-        public string LastSentMessageContent { get; set; }
+        public MessageInfo LastSentMessage { get; set; }
         public int GroupID { get; set; }
     }
 }
