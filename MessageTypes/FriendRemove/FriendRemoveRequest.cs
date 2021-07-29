a921972713fb0b2a82dc078f57375eaaf56cfd20
dian andreev georgiev <arrwblack@gmail.com>
2021-07-29 16:37:22 +0300

    bf

--- MessageTypes/FriendRemove/FriendRemoveRequest.cs
+++ MessageTypes/FriendRemove/FriendRemoveRequest.cs
@@ -5,10 +5,10 @@ namespace Chat_Application_Library
     public class FriendRemoveRequest : Message
     {
         public string Username { get; set; }
-
         public string DisplayName { get; set; }
-
         public string UserCode { get; set; }
+        public string Avatar { get; set; }
+        public int GroupID { get; set; }
         public FriendRemoveRequest()
         {
             Type = Type.Request;
