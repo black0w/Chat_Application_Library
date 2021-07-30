ac1139f7a45e3ef6529a6b1615b509e318ae25cd
dian andreev georgiev <arrwblack@gmail.com>
2021-07-30 13:21:02 +0300

    bf

--- Info/MessageInfo.cs
+++ Info/MessageInfo.cs
@@ -7,15 +7,11 @@ namespace Application_Library
     public class MessageInfo
     {
         public string SenderDisplayName { get; set; }
-
         public string SenderUserCode { set; get; }
-
         public DateTime TimeStamp { get; set; }
-
+        public string Avatar { get; set; }
         public InfoType messageType { get; set; }
-
         public string fileContent { get; set; }
-
         public string Content { get; set; }
     }
 }
