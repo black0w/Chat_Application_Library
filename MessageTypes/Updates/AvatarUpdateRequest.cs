5ff66a35d1a2401e1591d65d287b2138ce2ec85f
dian andreev georgiev <arrwblack@gmail.com>
2021-08-05 18:20:20 +0300

    added avatar update/ display name update / password update requests

---
+++ MessageTypes/Updates/AvatarUpdateRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chat_Application_Library
+{
+   public class AvatarUpdateRequest : Message
+    {
+        public string Username       { get; set; }
+        public string Avatar         { get; set; }
+
+        public AvatarUpdateRequest()
+        {
+            Type = Type.Request;
+            Action = "AvatarUpdate";
+        }
+    }
+}
---
+++ MessageTypes/Updates/DisplayNameUpdateRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chat_Application_Library
+{
+   public class DisplayNameUpdateRequest : Message
+    {
+        public string Username          { get; set; }
+        public string NewDisplayName    { get; set; }
+
+        public DisplayNameUpdateRequest()
+        {
+            Type = Type.Request;
+            Action = "AvatarUpdate";
+        }
+    }
+}
